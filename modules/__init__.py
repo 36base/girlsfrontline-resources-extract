96557afef7c12eaa7ab968bda1e4e344455de028
krepe-suZette <37508580+krepe-suZette@users.noreply.github.com>
2018-08-02 16:46:22 +0900

    Initializing

--- modules/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ['alpha', 'rename', 'imgcut', 'alpha']
--- modules/gfl-core.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open("data/doll.json", 'r', encoding="utf-8") as f:
-    core = json.loads(f.read())
-
-
-def name2id(name):
-    pass
